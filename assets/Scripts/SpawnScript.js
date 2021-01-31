de2fd4792cacf89591c5a8d96ad50cd9157c8cef
BomGame <57499805+BomGame@users.noreply.github.com>
2021-01-31 12:22:50 +0500

    vertical drop

--- assets/Scripts/SpawnScript.js
+++ assets/Scripts/SpawnScript.js
@@ -6,20 +6,43 @@ cc.Class({
             default: null,
             type: cc.Prefab,
           },
+        isSpawn: boolean = true,
+
     },
     
     // LIFE-CYCLE CALLBACKS:
 
-    // onLoad () {},
+    onLoad () {
+        cc.director.getCollisionManager().enabled = true;
+        isSpawn=true;
+    },
 
-    start () {
+    onBeginContact: function (contact, selfCollider, otherCollider) {
+        isSpawn=false;
+        otherCollider.body.linearVelocity=cc.v2(0,-30);
+    },
+
+    onEndContact (contact, selfCollider, otherCollider)
+    {
         var scene = cc.director.getScene();
         var Diamond = cc.instantiate(this.diamond);
         Diamond.parent = scene;
-        var node = this.node;
-        Diamond.setPosition(node.x, node.y);
+        Diamond.setPosition(selfCollider.node.x, selfCollider.node.y);
         Diamond.name='diam';
+        Diamond.color=cc.Color.CYAN;
     },
 
-    // update (dt) {},
+    start () { },
+
+    update (dt) {
+        if(isSpawn)
+        {
+            var scene = cc.director.getScene();
+            var Diamond = cc.instantiate(this.diamond);
+            Diamond.parent = scene;
+            var node = this.node;
+            Diamond.setPosition(node.x, node.y);
+            Diamond.name='diam';
+        }
+    },
 });
